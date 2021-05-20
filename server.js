7c1396cdaad5ff8d81ef8c37fb5a50e752062d9c
Nikolaus Brodfuehrer <nbrof@Nikolauss-MacBook-Air.local>
2021-05-20 18:27:27 -0400

    First Commit

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(cors());
 app.use(express.json()); 
 app.use(morgan("tiny")); 
 
+app.get('/', (req, res) => {
+    res.json({
+        status:200,
+        msg: 'You have hit the default route'
+    })
+})
+
 
 app.listen(PORT, () => {
     console.log(`Listening in on port ${PORT}`)
