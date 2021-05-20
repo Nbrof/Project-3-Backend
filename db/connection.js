c4247c593004366034b4bf6980a168cd434f2f11
Nikolaus Brodfuehrer <nbrof@Nikolauss-MacBook-Air.local>
2021-05-20 18:24:48 -0400

    First Commit

---
+++ db/connection.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+
+const mongoose = require("mongoose");
+
+
+const uri = process.env.MONGODB_URI;
+
+
+const config = { 
+    useUnifiedTopology: true, 
+    useNewUrlParser: true };
+
+mongoose.connect(uri, config);
+
+
+mongoose.connection
+  .on("open", () => console.log("MONGO CONNECTION IS OPEN"))
+  .on("close", () => console.log("MONGO CONNECTION IS CLOSED"))
+  .on("error", (error) =>
+    console.log("MONGO CONNECTION ERROR \n***************\n", error)
+  );
+
+module.exports = mongoose;
