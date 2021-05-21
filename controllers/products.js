84dfe2492244182c30239748006b26227e77ccc3
Nikolaus Brodfuehrer <nbrof@Nikolauss-MacBook-Air.local>
2021-05-21 17:11:36 -0400

    Changes syntax

--- controllers/products.js
+++ controllers/products.js
@@ -9,8 +9,8 @@ const productsSeed = [
   {
     name: "Chocolate Crunch",
     type: "Filler",
-    dairy: False,
-    toppings: False,
+    dairy: false,
+    toppings: false,
     description: "A tasty Chocolate treat!",
     img: "https://www.notion.so/Design-Assets-4541dddeab9a4707bfbdd56af7fb80b2#8913bc2cf66245f4b11963f0ec2d3eb8",
     price: "3,99$",
