00c87aab94074cd93b555624473eb3fd63b73687
cphork <christina.phork@gmail.com>
2021-05-21 17:17:33 -0500

    made changes to products

--- controllers/products.js
+++ controllers/products.js
@@ -11,15 +11,15 @@ const productsSeed = [
     dairy: false,
     toppings: false,
     description: "A tasty Chocolate treat!",
-    img: "https://www.notion.so/Design-Assets-4541dddeab9a4707bfbdd56af7fb80b2#8913bc2cf66245f4b11963f0ec2d3eb8",
-    price: "3,99$",
+    img: "https://images.unsplash.com/photo-1558500113-0d9cc3fa25ca?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTl8fGljZSUyMGNyZWFtfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    price: "$3.99",
     rating: "9/10",
   },
 ];
 
 //Seed Route
 
-router.get("/", async (req, res) => {
+router.get("/seed", async (req, res) => {
   try {
     await Products.remove({});
     await Products.create(productsSeed);
