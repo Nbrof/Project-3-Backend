74838ea958acefb67cac70d632194ab359067bed
Nikolaus Brodfuehrer <nbrof@Nikolauss-MacBook-Air.local>
2021-05-26 16:17:11 -0400

    Backend work

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 const iceRouter = require('./controllers/products')
 const signRouter = require('./controllers/signups')
 const logRouter = require('./controllers/login')
+const parlourRouter = require('./controllers/parlour')
 
 app.use(cors()); 
 app.use(express.json()); 
@@ -26,6 +27,7 @@ app.get('/', (req, res) => {
 app.use("/icecream", iceRouter);
 app.use("/signup", signRouter);
 app.use("/login", logRouter)
+app.use("/parlour", parlourRouter)
 
 
 app.listen(PORT, () => {
